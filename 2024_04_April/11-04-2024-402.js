f80ada5c5d57762c5422ca5ab7a3fae7e7ed8a56
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2024-04-11 08:10:14 -0400

    After an hour had to look at a solution... learned about monotonic stacks

---
+++ 2024_04_April/11-04-2024-402.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigit/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function (num, k) {
+  if (num.length == k) return '0'
+  const res = []
+  for (let i = 0; i < num.length; i++) {
+    while (k > 0 && res.length > 0 && res[res.length - 1] > num[i]) {
+      res.pop()
+      k--
+    }
+
+    if (res.length == 0 && num[i] == '0') continue
+    res.push(num[i])
+  }
+
+  while (k > 0 && res.length > 0) {
+    k--;
+    res.pop();
+  }
+
+  return res.length == 0 ? '0' : res.join('')
+}; s = function (num, k) {
+
+};
